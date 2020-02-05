1f8e0c3670491c23b82d92889683ca140a2a7ec9
Tine Kleivane <tine.kleivane@bekk.no>
2020-02-05 21:30:23 +0100

    Førsteutkast til prod

---
+++ immutable-webapp-prod/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "eu-north-1"
+  version = "~> 2.47"
+}
+
+resource "aws_s3_bucket" "prod" {
+  bucket = "tf-immutable-webapp-prod"
+  acl    = "public-read"
+
+  tags = {
+    Name        = "prod"
+    Environment = "prod"
+  }
+}
+
+module "immutable_cloudfront" {
+  source = "../immutable-webapp-common"
+
+  bucket_origin_id = "S3-${aws_s3_bucket.prod.id}"
+  bucket_domain_name = aws_s3_bucket.prod.bucket_regional_domain_name
+  environment= "prod"
+}
+
+output "bucket_asset" {
+  value = module.immutable_cloudfront.bucket.id
+}
+
+output "bucket_prod" {
+  value = aws_s3_bucket.prod.id
+}
+
+output "domain_name_prod" {
+  value = module.immutable_cloudfront.distribution.domain_name
+}
