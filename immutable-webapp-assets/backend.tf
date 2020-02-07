04c24c897bc3f8547904123e9739aef8d70cc71f
Tine Kleivane <tine.kleivane@bekk.no>
2020-02-07 13:49:00 +0100

    Block public access til tfstate-bucket

--- immutable-webapp-assets/backend.tf
+++ immutable-webapp-assets/backend.tf
@@ -14,6 +14,16 @@ resource "aws_s3_bucket" "tf-state-storage-s3" {
     }
 }
 
+resource "aws_s3_bucket_public_access_block" "tf-state-storage-s3" {
+  bucket = aws_s3_bucket.tf-state-storage-s3.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+
+}
+
 terraform {
   backend "s3" {
     bucket = "tf-immutable-infrastructure-remote-state-storage"
