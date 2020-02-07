f34f92191b3945ca0983955726f4cca8ea2895da
Tine Kleivane <tine.kleivane@bekk.no>
2020-02-07 13:12:04 +0100

    Oppdater tilsvarende endringer i prod

--- immutable-webapp-prod/main.tf
+++ immutable-webapp-prod/main.tf
@@ -14,15 +14,15 @@ resource "aws_s3_bucket" "prod" {
 }
 
 module "immutable_cloudfront" {
-  source = "../immutable-webapp-common"
+  source = "../modules/immutable-webapp-common"
 
   bucket_origin_id = "S3-${aws_s3_bucket.prod.id}"
   bucket_domain_name = aws_s3_bucket.prod.bucket_regional_domain_name
   environment= "prod"
 }
 
-output "bucket_asset" {
-  value = module.immutable_cloudfront.bucket.id
+output "bucket_id_asset" {
+  value = module.immutable_cloudfront.bucket_id
 }
 
 output "bucket_prod" {
