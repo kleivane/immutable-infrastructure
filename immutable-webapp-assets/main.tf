db8db3e3225f8a860e6fa9f5fc8032f99259eac5
Tine Kleivane <tine.kleivane@bekk.no>
2020-02-07 12:52:14 +0100

    Ny output

--- immutable-webapp-assets/main.tf
+++ immutable-webapp-assets/main.tf
@@ -11,6 +11,10 @@ resource "aws_s3_bucket" "assets" {
   }
 }
 
-output "bucket_asset" {
+output "id" {
   value = aws_s3_bucket.assets.id
+}
+
+output "bucket_regional_domain_name" {
+  value = aws_s3_bucket.assets.bucket_regional_domain_name
 }
